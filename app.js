e365f58ddb19424c7ed0fe8487e161b209059c80
kazi-bablu <kazibablubif@gmail.com>
2018-09-24 02:19:46 +0600

    working on calcuation

--- app.js
+++ app.js
@@ -10,6 +10,55 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack:function () {
+            this.monsterHealth  -= this.calculateDamage(3,10)
+
+            if (this.monsterHealth <= 0){
+                alert('you won!');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            if (this.checkWin()){
+                return;
+            }
+            this.playerHealth  -= this.calculateDamage(5,12)
+
+            if (this.playerHealth <=0){
+                alert('you lost!');
+                this.gameIsRunning = false;
+            }
+            this.checkWin();
+        },
+        specialAttack:function () {
+            
+        },
+        heal:function () {
+            
+        },
+        giveUp:function () {
+            
+        },
+        calculateDamage:function (min, max) {
+            return Math.max(Math.floor(Math.random()* max)+1,min);
+        },
+        checkWin:function () {
+            if (this.monsterHealth <= 0){
+                if (confirm('you won! New Game?')){
+                    this.startGame();
+                } else{
+                    this.gameIsRunning = false;
+                }
+            }else if (this.playerHealth <= 0){
+                if (confirm('you lost! new game')){
+                    this.startGame();
+                } else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 });
